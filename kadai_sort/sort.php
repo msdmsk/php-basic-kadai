eee1d1fe743c6d455fdc4fcc934700bde63c0deb
msdmsk <msdmsk0224@gmail.com>
2023-11-22 23:46:50 +0900

    sort

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -9,12 +9,29 @@
 <body>
     <p>
         <?php
-        // ソートする配列を宣言
-        $nums = [15, 4, 18, 23, 10 ];
+            // ソートする配列を宣言
+            $nums = [15, 4, 18, 23, 10 ];
+            $array = $nums;
 
-        function sort_2way($array, $order) {
-            arsort($nums);
-        }
+            function sort_2way($array, $order) {
+
+                if ($order) {
+                    echo "昇順にソートします。<br>";
+                    sort($array);
+                    foreach ($array as $array) {
+                        echo $array . "<br>";
+                    }
+                } else {
+                    echo "降順にソートします。<br>";
+                    rsort($array);
+                    foreach ($array as $array) {
+                        echo $array . "<br>";
+                    }
+                }
+            }
+
+            echo sort_2way($array, TRUE);
+            echo sort_2way($array, FALSE);
         ?>
     </p>
 </body>
